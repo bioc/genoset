b49dc20c62c9fe187d6b8e97c2fc90a7300bdbe8
Peter M Haverty <phaverty@gene.com>
2012-01-21 03:03:44 +0000

    Almost done converting to bioc style tests
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/genoset@62191 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/prove.pmh.utils.R
+++ tests/prove.pmh.utils.R
@@ -1,24 +1,2 @@
-#!/usr/bin/env Rscript
-#####
-#####  A script to run tests on the genoset package
-
-suppressMessages(library(genoset))
-suppressMessages(library(RUnit))
-suppressMessages(library(bigmemory))
-
-options(warn=1)
-RUnit = options("RUnit")$RUnit
-RUnit$silent = TRUE
-options("RUnit"=RUnit)
-
-testsuite.genoset <- defineTestSuite("genoset.test.suite", 
-                                       dirs = "unit",
-                                       testFileRegexp = "^runit.+\\.R", 
-                                       testFuncRegexp = "^test.+", 
-                                       rngKind = "default",
-                                       rngNormalKind = "default")
-
-results <- capture.output(runTestSuite(testsuite.genoset))
-
-## Finish up
-quit(runLast=F)
+require("genoset") || stop("unable to load genoset package")
+genoset:::.test()
