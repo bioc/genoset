917b8802a5249006c922b37876018d9bce4ba58f
Peter M Haverty <phaverty@gene.com>
2012-06-28 19:56:47 +0000

    Reverted to findOverlaps for segs2Rle until I can debug a weird corner case for boundingIncdicesByChr as used in bounds2Rle
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/genoset@67126 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/genoset-class.R
+++ R/genoset-class.R
@@ -1157,8 +1157,9 @@ segs2Rle <- function(segs, locs) {
 #  } else {
     seg.gr = GRanges( ranges=IRanges(start=segs[,"loc.start"], end=segs[,"loc.end"]),
       seqnames=segs[,"chrom"], "Value"=segs[,"seg.mean"])
-    bounds = boundingIndicesByChr( seg.gr, locs )
-    temp.rle = bounds2Rle( bounds, values(seg.gr)$Value, nrow(locs) )
+    temp.rle = Rle(values(seg.gr)$Value[match(locs, seg.gr)])
+#    bounds = boundingIndicesByChr( seg.gr, locs )
+#    temp.rle = bounds2Rle( bounds, values(seg.gr)$Value, nrow(locs) )
 #  }
   return(temp.rle)
 }
