9b0abadd37987afa01bad3b693408c3b107dc126
Peter M Haverty <phaverty@gene.com>
2014-04-05 06:35:52 +0000

    Version: 1.15.14. Dumping RangedData stuff from tests
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/genoset@88468 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_bounds.R
+++ inst/unitTests/test_bounds.R
@@ -70,30 +70,27 @@ test_rangeSampleMeans <- function() {
   probe.names = letters[1:10]
  
   subject = GenoSet(
-    locData=RangedData(ranges=IRanges(start=1:10,width=1,names=probe.names),space=c(rep("chr1",4),rep("chr3",2),rep("chrX",4)),universe="hg18"),
+    locData=GRanges(ranges=IRanges(start=1:10,width=1,names=probe.names),seqnames=c(rep("chr1",4),rep("chr3",2),rep("chrX",4))),
     cn=matrix(31:60,nrow=10,ncol=3,dimnames=list(probe.names,test.sample.names)),
     pData=data.frame(matrix(LETTERS[1:15],nrow=3,ncol=5,dimnames=list(test.sample.names,letters[1:5]))),
     annotation="SNP6"
     )
 
-  query.rd = RangedData( ranges=IRanges(start=c(2,3,7,8),width=2,names=c("joe","bob","fred","tom")), space=factor(c("chr1","chr1","chrX","chrX"),levels=c("chr1","chrX")),universe="hg18")
+  query.gr = GRanges( ranges=IRanges(start=c(2,3,7,8),width=2,names=c("joe","bob","fred","tom")), seqnames=factor(c("chr1","chr1","chrX","chrX"),levels=c("chr1","chrX")))
 
-  means = matrix(c(32,42,52,33,43,53,37,47,57,38,48,58)+0.5,ncol=nrow(query.rd),nrow=ncol(subject),dimnames=list(sampleNames(subject),rownames(query.rd)))
+  means = matrix(c(32,42,52,33,43,53,37,47,57,38,48,58)+0.5,ncol=nrow(query.gr),nrow=ncol(subject),dimnames=list(sampleNames(subject),rownames(query.gr)))
   means = t(means)
-  checkEquals( rangeSampleMeans( query.rd, subject, "cn" ), means)
+  checkEquals( rangeSampleMeans( query.gr, subject, "cn" ), means)
   
   rle.genoset = GenoSet(
-    locData=RangedData(ranges=IRanges(start=1:10,width=1,names=probe.names),space=c(rep("chr1",4),rep("chr3",2),rep("chrX",4)),universe="hg18"),
+    locData=GRanges(ranges=IRanges(start=1:10,width=1,names=probe.names),seqnames=c(rep("chr1",4),rep("chr3",2),rep("chrX",4))),
     cn=DataFrame(K=Rle(1:10),L=Rle(11:20),M=Rle(21:30),row.names=probe.names),
     pData=data.frame(matrix(LETTERS[1:15],nrow=3,ncol=5,dimnames=list(test.sample.names,letters[1:5]))),
     annotation="SNP6"
     )
-  rle.means = matrix(c(2.5,3.5,7.5,8.5,12.5,13.5,17.5,18.5,22.5,23.5,27.5,28.5), nrow=nrow(query.rd), ncol=ncol(rle.genoset), dimnames=list(rownames(query.rd),sampleNames(rle.genoset)))
-  checkEquals( rangeSampleMeans( query.rd, rle.genoset, "cn" ), rle.means, "DataFrame of Rle")
+  rle.means = matrix(c(2.5,3.5,7.5,8.5,12.5,13.5,17.5,18.5,22.5,23.5,27.5,28.5), nrow=nrow(query.gr), ncol=ncol(rle.genoset), dimnames=list(rownames(query.gr),sampleNames(rle.genoset)))
+  checkEquals( rangeSampleMeans( query.gr, rle.genoset, "cn" ), rle.means, "DataFrame of Rle")
 
-  # With GRanges
-  query.gr = as(query.rd,"GRanges")
-  locData(subject) = as(locData(subject),"GRanges")
   checkEquals( rangeSampleMeans( query.gr, subject, "cn" ), means)
   locData(rle.genoset) = as(locData(rle.genoset),"GRanges")
   checkEquals( rangeSampleMeans( query.gr, rle.genoset, "cn" ), rle.means, "DataFrame of Rle")
@@ -123,22 +120,22 @@ test_rangeColMeans <- function() {
 }
 
 test_boundingIndicesByChr <- function() {
-  subject= RangedData(ranges=IRanges(start=c(seq(from=10,to=40,by=10),seq(from=110,to=140,by=10),seq(from=1110,to=1140,by=10)),width=2,names=as.character(1:12)),
-    space=c(rep("1",4),rep("2",4),rep("3",4)))
-  query = RangedData(ranges=IRanges(start=c(2,9,39,50,102,109,139,150,1102,1109,1139,1150),width=2,names=as.character(1:12)),space=c(rep("1",4),rep("2",4),rep("3",4)))
+  subject= GRanges(ranges=IRanges(start=c(seq(from=10,to=40,by=10),seq(from=110,to=140,by=10),seq(from=1110,to=1140,by=10)),width=2,names=as.character(1:12)),
+    seqnames=c(rep("1",4),rep("2",4),rep("3",4)))
+  query = GRanges(ranges=IRanges(start=c(2,9,39,50,102,109,139,150,1102,1109,1139,1150),width=2,names=as.character(1:12)),seqnames=c(rep("1",4),rep("2",4),rep("3",4)))
   res = matrix(as.integer(c(1,1, 1,1, 3,4, 4,4, 5,5, 5,5, 7,8, 8,8, 9,9, 9,9, 11,12, 12,12)),byrow=TRUE,ncol=2,dimnames=list(rownames(query),c("left","right")))
   checkIdentical(res, boundingIndicesByChr(query,subject))
 
   subject2= GRanges(ranges=IRanges(start=c(seq(from=10,to=40,by=10),seq(from=110,to=140,by=10),seq(from=1110,to=1140,by=10)),width=2,names=as.character(1:12)),
     seqnames=c(rep("1",4),rep("2",4),rep("5",4)))
-  query2 = RangedData(ranges=IRanges(start=c(2,9,39,50,102,109,139,150,1102,1109,1139,1150),width=2,names=as.character(1:12)),space=c(rep("1",4),rep("3",4),rep("5",4)))
+  query2 = GRanges(ranges=IRanges(start=c(2,9,39,50,102,109,139,150,1102,1109,1139,1150),width=2,names=as.character(1:12)),seqnames=c(rep("1",4),rep("3",4),rep("5",4)))
   query2 = query2[chr(query2) %in% c("1", "5"), ]
   res2 = res[c(1:4,9:12),]
   checkIdentical(res2, boundingIndicesByChr(query2,subject2))
   
-  subject3 = RangedData(ranges=IRanges(start=c(seq(from=10,to=40,by=10),seq(from=110,to=140,by=10),seq(from=1110,to=1140,by=10)),width=1,names=as.character(1:12)),
-    space=c(rep("1",4),rep("2",4),rep("3",4)))
-  query3 = RangedData(ranges=IRanges(start=c(2,9,39,50,102,109,139,150,1102,1110,1139,1150),width=c(rep(2,9),1,2,2),names=as.character(1:12)),space=c(rep("1",4),rep("2",4),rep("3",4)))
+  subject3 = GRanges(ranges=IRanges(start=c(seq(from=10,to=40,by=10),seq(from=110,to=140,by=10),seq(from=1110,to=1140,by=10)),width=1,names=as.character(1:12)),
+    seqnames=c(rep("1",4),rep("2",4),rep("3",4)))
+  query3 = GRanges(ranges=IRanges(start=c(2,9,39,50,102,109,139,150,1102,1110,1139,1150),width=c(rep(2,9),1,2,2),names=as.character(1:12)),seqnames=c(rep("1",4),rep("2",4),rep("3",4)))
   res3 = matrix(as.integer(c(1,1, 1,1, 3,4, 4,4, 5,5, 5,5, 7,8, 8,8, 9,9, 9,9, 11,12, 12,12)),byrow=TRUE,ncol=2,dimnames=list(rownames(query3),c("left","right")))
   checkIdentical(res3, boundingIndicesByChr(query3,subject3))
   
--- inst/unitTests/test_segments.R
+++ inst/unitTests/test_segments.R
@@ -3,35 +3,34 @@
 #########################################
 sample.names = LETTERS[11:13]
 probe.names = letters[1:10]
-locData.rd = RangedData(ranges=IRanges(start=c(1,3,5,7,4,6,2,4,6,8),width=1,names=probe.names),space=c(rep("chr1",4),rep("chr3",2),rep("chrX",4)),universe="hg18")
-locData.gr = as(locData.rd,"GRanges")
+locData.gr = GRanges(ranges=IRanges(start=c(1,3,5,7,4,6,2,4,6,8),width=1,names=probe.names),seqnames=c(rep("chr1",4),rep("chr3",2),rep("chrX",4)))
 basic.rle.df = DataFrame(
   K = Rle(c(5.3,2.3,1.20),c(4,2,4)),
   L = Rle(c(1.1,1.4,2.2,3.3,0.5),c(1,3,2,2,2)),
   M = Rle(c(3.3,4.3,4.3,6.3,7.3),c(1,3,1,1,4)),
-    row.names=rownames(locData.rd))
+    row.names=rownames(locData.gr))
 
 basic.segs = list(
-  K = data.frame( ID = "K", chrom = factor(c("chr1","chr3","chrX"),levels=names(locData.rd)), loc.start = c(1,1,1), loc.end = c(10,10,10),
+  K = data.frame( ID = "K", chrom = factor(c("chr1","chr3","chrX"),levels=names(locData.gr)), loc.start = c(1,1,1), loc.end = c(10,10,10),
     num.mark = c(4,2,4), seg.mean = c(5.3,2.3,1.2), stringsAsFactors=FALSE ),
-  L = data.frame( ID = "L", chrom = factor(c("chr1","chr1","chr3","chrX","chrX"),levels=names(locData.rd)), loc.start = c(1,3,1,1,5), loc.end = c(2,10,10,4,10),
+  L = data.frame( ID = "L", chrom = factor(c("chr1","chr1","chr3","chrX","chrX"),levels=names(locData.gr)), loc.start = c(1,3,1,1,5), loc.end = c(2,10,10,4,10),
     num.mark = c(1,3,2,2,2), seg.mean = c(1.1,1.4,2.2,3.3,0.5), stringsAsFactors=FALSE ),
-  M = data.frame( ID = "M", chrom = factor(c("chr1","chr1","chr3","chr3","chrX"),levels=names(locData.rd)), loc.start = c(1,3,4,5,1), loc.end = c(2,10,5,6,10),
+  M = data.frame( ID = "M", chrom = factor(c("chr1","chr1","chr3","chr3","chrX"),levels=names(locData.gr)), loc.start = c(1,3,4,5,1), loc.end = c(2,10,5,6,10),
     num.mark = c(1,3,1,1,4), seg.mean = c(3.3,4.3,4.3,6.3,7.3), stringsAsFactors=FALSE )
   )
 
 basic.segs.after = list(
-  K = data.frame( chrom = factor(c("chr1","chr3","chrX"),levels=names(locData.rd)),
+  K = data.frame( chrom = factor(c("chr1","chr3","chrX"),levels=chr(locData.gr)),
     loc.start = c(1,4,2), loc.end = c(7,6,8), num.mark = c(4,2,4), seg.mean = c(5.3,2.3,1.2), stringsAsFactors=FALSE ),
-  L = data.frame( chrom = factor(c("chr1","chr1","chr3","chrX","chrX"),levels=names(locData.rd)),
+  L = data.frame( chrom = factor(c("chr1","chr1","chr3","chrX","chrX"),levels=chr(locData.gr)),
     loc.start = c(1,3,4,2,6), loc.end = c(1,7,6,4,8), num.mark = c(1,3,2,2,2), seg.mean = c(1.1,1.4,2.2,3.3,0.5), stringsAsFactors=FALSE ),
-  M = data.frame( chrom = factor(c("chr1","chr1","chr3","chr3","chrX"),levels=names(locData.rd)),
+  M = data.frame( chrom = factor(c("chr1","chr1","chr3","chr3","chrX"),levels=chr(locData.gr)),
     loc.start = c(1,3,4,6,2), loc.end = c(1,7,4,6,8), num.mark = c(1,3,1,1,4), seg.mean = c(3.3,4.3,4.3,6.3,7.3), stringsAsFactors=FALSE )
   )
 
 stacked.basic.segs.after = data.frame(
   Sample=c(rep("K",3),rep("L",5),rep("M",5)),
-  chrom = factor(c("chr1","chr3","chrX","chr1","chr1","chr3","chrX","chrX","chr1","chr1","chr3","chr3","chrX"),levels=names(locData.rd)),
+  chrom = factor(c("chr1","chr3","chrX","chr1","chr1","chr3","chrX","chrX","chr1","chr1","chr3","chr3","chrX"),levels=names(locData.gr)),
   loc.start = c(1,4,2,1,3,4,2,6,1,3,4,6,2), loc.end = c(7,6,8,1,7,6,4,8,1,7,4,6,8),
   num.mark = c(4,2,4,1,3,2,2,2,1,3,1,1,4),
   seg.mean = c(5.3,2.3,1.2,1.1,1.4,2.2,3.3,0.5,3.3,4.3,4.3,6.3,7.3),
@@ -39,30 +38,24 @@ stacked.basic.segs.after = data.frame(
   stringsAsFactors=FALSE)
 
 basic.rds.after = list(
-  K = RangedData( ranges=IRanges(start = c(1,4,2), end = c(7,6,8)),space = factor(c("chr1","chr3","chrX"),levels=names(locData.rd)),  score = c(5.3,2.3,1.2), num.mark = c(4,2,4) ),
-  L = RangedData( ranges=IRanges(start = c(1,3,4,2,6), end = c(1,7,6,4,8)), space = factor(c("chr1","chr1","chr3","chrX","chrX"),levels=names(locData.rd)),
+  K = GRanges( ranges=IRanges(start = c(1,4,2), end = c(7,6,8)),seqnames = factor(c("chr1","chr3","chrX"),levels=names(locData.rd)),  score = c(5.3,2.3,1.2), num.mark = c(4,2,4) ),
+  L = GRanges( ranges=IRanges(start = c(1,3,4,2,6), end = c(1,7,6,4,8)), seqnames = factor(c("chr1","chr1","chr3","chrX","chrX"),levels=names(locData.rd)),
     score = c(1.1,1.4,2.2,3.3,0.5), num.mark = c(1,3,2,2,2)),
-  M = RangedData( ranges=IRanges(start = c(1,3,4,6,2), end = c(1,7,4,6,8)), space = factor(c("chr1","chr1","chr3","chr3","chrX"),levels=names(locData.rd)),
+  M = GRanges( ranges=IRanges(start = c(1,3,4,6,2), end = c(1,7,4,6,8)), seqnames = factor(c("chr1","chr1","chr3","chr3","chrX"),levels=names(locData.rd)),
     score = c(3.3,4.3,4.3,6.3,7.3), num.mark = c(1,3,1,1,4))
   )
 
-test_segs2RangedData <- function() {
-  checkEquals( segs2RangedData(basic.segs.after$K), basic.rds.after$K )
-  checkEquals( segs2RangedData(basic.segs.after$L), basic.rds.after$L )
-  checkEquals( segs2RangedData(basic.segs.after$M), basic.rds.after$M )
+test_segs2GRanges <- function() {
+  checkEquals( segs2Granges(basic.segs.after$K), basic.rds.after$K )
+  checkEquals( segs2Granges(basic.segs.after$L), basic.rds.after$L )
+  checkEquals( segs2Granges(basic.segs.after$M), basic.rds.after$M )
 }
 
 test_segs2Rle <- function() {
-  # With RangedData
-  checkEquals( segs2Rle( basic.segs[[1]], locData.rd ), basic.rle.df[[1]], checkNames=FALSE )
-  checkEquals( segs2Rle( basic.segs[[2]], locData.rd ), basic.rle.df[[2]], checkNames=FALSE )
-  checkEquals( segs2Rle( basic.segs[[3]], locData.rd ), basic.rle.df[[3]], checkNames=FALSE )
-  na.df = data.frame( chrom = factor(c("chr1","chr1","chr3","chr3","chrX"),levels=names(locData.rd)),
-    loc.start = c(3,5,4,6,2), loc.end = c(3,7,4,6,6), num.mark = c(1,2,1,1,3), seg.mean = c(3.3,4.3,4.3,6.3,7.3), stringsAsFactors=FALSE )
-  na.rle = Rle( c(NA,3.3,4.3,6.3,7.3,NA), c(1,1,3,1,3,1) )
-  checkEquals( segs2Rle( na.df, locData.rd ), na.rle , checkNames=FALSE )
-
   # With GRanges
+  na.df = data.frame( chrom = factor(c("chr1", "chr1", "chr3", "chr3", "chrX"), levels=names(locData.rd)),
+    loc.start = c(3, 5, 4, 6, 2),  loc.end = c(3, 7, 4, 6, 6),  num.mark = c(1, 2, 1, 1, 3),  seg.mean = c(3.3, 4.3, 4.3, 6.3, 7.3),  stringsAsFactors=FALSE )
+  na.rle = Rle( c(NA, 3.3, 4.3, 6.3, 7.3, NA),  c(1, 1, 3, 1, 3, 1) )
   checkEquals( segs2Rle( basic.segs[[1]], locData.gr ), basic.rle.df[[1]], checkNames=FALSE )
   checkEquals( segs2Rle( basic.segs[[2]], locData.gr ), basic.rle.df[[2]], checkNames=FALSE )
   checkEquals( segs2Rle( basic.segs[[3]], locData.gr ), basic.rle.df[[3]], checkNames=FALSE )
@@ -79,14 +72,6 @@ test_segTable <- function() {
   start = start(locData.rd)
   end = end(locData.rd)
 
-  # With RangedData
-  checkEquals( segTable( basic.rle.df[["K"]], locData.rd), basic.segs.after[["K"]], checkNames=FALSE )
-  checkEquals( segTable( basic.rle.df[["L"]], locData.rd), basic.segs.after[["L"]], checkNames=FALSE )
-  checkEquals( segTable( basic.rle.df[["M"]], locData.rd), basic.segs.after[["M"]], checkNames=FALSE )
-  checkEquals( segTable( basic.rle.df[["M"]], chr.ind=chr.ind, start=start, end=end), basic.segs.after[["M"]], checkNames=FALSE, "segTable on Rle providing chr.ind, start, end" )
-  checkEquals( segTable( basic.rle.df, locData.rd ), basic.segs.after, checkNames=FALSE )
-  checkEquals( segTable( basic.rle.df, locData.rd, stack=TRUE ), stacked.basic.segs.after, checkNames=FALSE )
-
   # With GRanges
   checkEquals( segTable( basic.rle.df[["K"]], locData.gr), basic.segs.after[["K"]], checkNames=FALSE )
   checkEquals( segTable( basic.rle.df[["L"]], locData.gr), basic.segs.after[["L"]], checkNames=FALSE )
@@ -101,9 +86,8 @@ test_segPairTable <- function() {
   loh = Rle(c(2,4,6,8,10,12),rep(2,6))
   start = c(9:11,4:9,15:17)
   end = start
-  locs.rd = RangedData(IRanges(start=start,end=end),space=c(rep("chr1",3),rep("chr2",6),rep("chr3",3)))
-  locs.gr = as(locs.rd,"GRanges")
-  chr.ind = chrIndices(locs.rd)
+  locs.gr = GRanges(IRanges(start=start,end=end),seqnames=c(rep("chr1",3),rep("chr2",6),rep("chr3",3)))
+  chr.ind = chrIndices(locs.gr)
 
   # Test returning a GRanges from segPairTable
 #  segs.gr = GRanges(
@@ -132,7 +116,6 @@ test_segPairTable <- function() {
   loh.df = DataFrame(a=loh,b=loh+1)
   stacked.segs.df = do.call(rbind,list(a = segPairTable(cn,loh,chr.ind=chr.ind,start=start,end=end), b = segPairTable(cn+1,loh+1,chr.ind=chr.ind,start=start,end=end)))
   stacked.segs.df = cbind(Sample = rep(c("a","b"),each=8),stacked.segs.df,stringsAsFactors=FALSE,row.names=NULL)
-  checkEquals(stacked.segs.df, segPairTable(cn.df,loh.df,locs=locs.rd,stack=TRUE))
   checkEquals(stacked.segs.df, segPairTable(cn.df,loh.df,locs=locs.gr,stack=TRUE))
 }
 
@@ -159,23 +142,15 @@ test_runCBS <- function() {
   probe.names =  paste("p",1:30,sep="")
   ds = matrix(c(c(rep(5,20),rep(3,10)),c(rep(2,10),rep(7,10),rep(9,10))),ncol=2,dimnames=list(probe.names,sample.names))
   ds.with.na = matrix(c(c(rep(5,9),NA,rep(5,10),rep(3,10)),c(rep(2,10),rep(7,10),rep(9,10))),ncol=2,dimnames=list(probe.names,sample.names))
-  locs.rd = RangedData(ranges=IRanges(start=c(1:20,1:10),width=1,names=probe.names),space=paste("chr",c(rep(1,20),rep(2,10)),sep=""))
-  locs.gr = as(locs.rd,"GRanges")
+  locs.gr = GRanges(ranges=IRanges(start=c(1:20,1:10),width=1,names=probe.names),seqnames=paste("chr",c(rep(1,20),rep(2,10)),sep=""))
   
   seg.rle.result = DataFrame( a1 = Rle(c(rep(5,20),rep(3,10))), a2 = Rle(c(rep(2,10),rep(7,10),rep(9,10))), row.names=probe.names )
   seg.list.result = list(
     a1 = data.frame( ID=rep("a1",2), chrom=factor(c("chr1","chr2")), loc.start=c(1,1), loc.end=c(20,10), num.mark=c(20,10), seg.mean=c(5,3), stringsAsFactors=FALSE),
     a2 = data.frame( ID=rep("a2",3), chrom=factor(c("chr1","chr1","chr2")), loc.start=c(1,11,1), loc.end=c(10,20,10), num.mark=c(10,10,10), seg.mean=c(2,7,9), stringsAsFactors=FALSE)
     )
-  
-  # With RangedData
-  checkEquals( runCBS(ds,locs.rd, n.cores=8), seg.rle.result, "Return DF of Rle")
-  checkEquals( runCBS(ds.with.na,locs.rd, n.cores=8), seg.rle.result, "Return DF of Rle with some NA in starting data")
-  checkEquals( runCBS(ds,locs.rd, n.cores=8, return.segs=TRUE), seg.list.result, "Return seg dfs")
-  checkEquals( runCBS(seg.rle.result,locs.rd, n.cores=8), seg.rle.result, "Return seg dfs starting from DF of Rle (like mbaf)")
-  checkEquals( runCBS(ds,locs.rd, n.cores=8,alpha=0.01), seg.rle.result, "Runs OK with alpha at 0.01 (requires loading of data from DNAcopy)")
 
-  # With RangedData
+  # With GRanges
   checkEquals( runCBS(ds,locs.gr, n.cores=8), seg.rle.result, "Return DF of Rle")
   checkEquals( runCBS(ds.with.na,locs.gr, n.cores=8), seg.rle.result, "Return DF of Rle with some NA in starting data")
   checkEquals( runCBS(ds,locs.gr, n.cores=8, return.segs=TRUE), seg.list.result, "Return seg dfs")
