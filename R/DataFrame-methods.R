d7ac1aead4e3589e4c89c6d3d6ffeed1ba6ac4a5
Peter M Haverty <phaverty@gene.com>
2014-04-18 23:53:34 +0000

    Version: 1.17.3. Added beginnings of RleDataFrame, including pseudocode for rowMeans.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/genoset@89207 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/DataFrame-methods.R
+++ R/DataFrame-methods.R
@@ -1,5 +1,50 @@
 ##### Additional methods for DataFrame class to allow use of DataTable of Rle vectors as assayData element
 
+setClass("RleDataFrame",
+         representation(
+           rownames = "characterORNULL",
+           nrows = "integer"
+           ),
+         prototype(rownames = NULL,
+                   nrows = 0L,
+                   listData = structure(list(),  names = character())),
+         contains = c("SimpleRleList", "DataFrame")
+)
+
+RleDataFrame <- function(..., row.names=NULL, check.names=TRUE) {
+  x = DataFrame(..., row.names=row.names, check.names=check.names)
+  return( new("RleDataFrame", listData=x@listData, rownames=rownames(x), nrows=nrow(x)) )
+}
+
+setMethod("show", "RleDataFrame",
+          function(object) {
+            message(sprintf("RleDataFrame with %i rows and %i columns\n", nrow(object), ncol(object)))
+            show(object@listData)
+          })
+
+setGeneric("rowMeans", function(x, na.rm=TRUE, dims=1L) standardGeneric("colMeans") )
+setMethod("rowMeans", signature(x="RleDataFrame"),
+          function(x) {
+            # This will probably have to be .Call, but try in R first
+            # set counter for each col to first runLength - 1
+            # set vector to first runValues
+            # temp = sum vector of first runValues
+            # result[1] = temp
+            # for i in 2:nrows(x)
+            # for cols with zero counter
+            #      temp += (next runValue) - (current runValue)
+            #      set counter to next runLength
+            #      current runValue = next runValue
+            # res[i] = temp
+            # decrement all counters
+            # }
+            # divide by ncols at end
+            # NAs make this awful of course
+            # return(res)
+            ## Can use diff() to get running difference of runValues per Rle rather than doing this in the loop
+            ##   This will be pretty big, maybe just do it in the loop looking forward to C version
+          })
+
 ##' Means of columns
 ##'
 ##' Calculate means of columns of a DataFrame as if it were a matrix. Allows colmeans
@@ -16,7 +61,7 @@
 ##'  mat.ds = matrix( c(5,5,4,4,3,3,3,6,9,9,9,9), ncol=2, dimnames=list(NULL,c("a","b")))
 ##'  \dontrun{ identical( colMeans(df.ds), colMeans(mat.ds) ) }
 ##' @rdname colMeans
-setGeneric("colMeans", function(x,na.rm=TRUE,dims=1L) standardGeneric("colMeans") )
+setGeneric("colMeans", function(x, na.rm=TRUE, dims=1L) standardGeneric("colMeans") )
 ##' @rdname colMeans
 setMethod("colMeans", signature(x="DataFrame"), function(x,na.rm=TRUE,dims=1L) { return( sapply(x,mean,na.rm=na.rm) ) } )
 
--- inst/unitTests/test_DataFrame.R
+++ inst/unitTests/test_DataFrame.R
@@ -1,4 +1,17 @@
-#### Tests for additional methods for DataFrame
+#### Tests for RleDataFrame and  additional methods for DataFrame
+
+test_RleDataFrame <- function() {
+  foo = new("RleDataFrame", listData=list(A=Rle(1:5, rep(2, 5)), B=Rle(6:10,rep(2, 5))), nrows=10L)
+  foo2 = new("RleDataFrame", listData=list(A=Rle(1, 2), B=Rle(6,2)), nrows=2L)
+  foo3 = RleDataFrame(A=Rle(1:5, rep(2, 5)), B=Rle(6:10,rep(2, 5)))
+  checkEquals( foo[1:2, ], foo2, "Row subset")
+  checkEquals( foo, foo3, "Create RleDataFrame with new or with RleDataFrame" )
+  checkException(new("RleDataFrame", listData=list(1:10, 1:10), nrows=10L), silent=TRUE)
+}
+
+test_rowMeans <- function() {
+  
+}
 
 test_colMeans <- function() {
   df.ds = DataFrame( a = Rle(c(5,4,3),c(2,2,2)), b = Rle(c(3,6,9),c(1,1,4)) )
