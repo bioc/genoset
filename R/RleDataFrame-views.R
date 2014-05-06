bf2bfb6ed98ca58f80168c45f439c7989c69d49d
Peter M Haverty <phaverty@gene.com>
2014-05-06 04:05:47 +0000

    Version: 1.19.7. range* on RleDataFrame now does 'trim' once and goes straight to the C with internal functions of our own.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/genoset@89953 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/RleDataFrame-views.R
+++ R/RleDataFrame-views.R
@@ -17,7 +17,7 @@ NULL
 ##' constructed with \code{boundingIndicesByChr}.
 ##' @param na.rm scalar logical, ignore NAs in calculations?
 ##' @param simplify scalar logical, simplify result? For a single view, a vector, otherwise a matrix with one row per view.
-##' @param FUN S4 Generic function or scalar character with the name of an S4 Generic function
+##' @param RLEFUN function, internal rle view summary function like .rle_view_means
 ##' @param FUN.TYPE scalar character, the storage mode for the returned vector or matrix (when simplify==TRUE).
 ##' @export 
 ##' @return With \code{simplify == TRUE}, a vector for single view or a matrix
@@ -26,24 +26,29 @@ NULL
 ##' @rdname do_rledf_views
 ##' @seealso RleDataFrame boundingIndicesByChr
 ##' @family views
-.do_rledf_views <- function(x, ir, na.rm=FALSE, simplify=TRUE, FUN, FUN.TYPE=c("numeric", "double", "integer", "logical")) {
+.do_rledf_views <- function(x, ir, na.rm=FALSE, simplify=TRUE, RLEFUN, FUN.TYPE=c("numeric", "double", "integer", "logical")) {
+  # Make an IRanges from ranges matrix if necessary
   if (is.matrix(ir)) {
     ir = IRanges(start=ir[, 1], end=ir[, 2], names=rownames(ir))
   }
-  myfun = getMethod(FUN, "RleViews", where="IRanges")
+  # Trim IRanges once if necessary
+  start(ir)[ start(ir) < 1L ] = 1L
+  end(ir)[ end(ir) > nrow(x) ] = nrow(x)
+  # Hoist the Views dispatch
   myviewfun = getMethod("Views", "Rle", where="IRanges")
+  # Calculate the view stats
   if (simplify == TRUE) {
     FUN.TYPE = match.arg(FUN.TYPE)
     nviews = length(ir)
     val = vapply(x,   	 
            FUN=function(rle) {
-             myfun(myviewfun(rle, ir), na.rm=na.rm)
-           }, USE.NAMES=ifelse(nviews > 1, TRUE, FALSE),
+             RLEFUN(myviewfun(rle, ir), na.rm=na.rm)
+           }, USE.NAMES=nviews > 1, 
            FUN.VALUE=vector( FUN.TYPE, nviews ) )
   } else {
     val = lapply(x,   	 
       function(rle) {
-        myfun(myviewfun(rle, ir), na.rm=na.rm)
+        RLEFUN(myviewfun(rle, ir), na.rm=na.rm)
       })
     }
   return(val)
@@ -53,41 +58,49 @@ NULL
 setGeneric("rangeSums", function(x, ir, na.rm=TRUE, simplify=TRUE) { standardGeneric("rangeSums") })
 setMethod("rangeSums", signature=signature(x="RleDataFrame"), 
           function(x, ir, na.rm=TRUE, simplify=TRUE) {
-            .do_rledf_views(x, ir, na.rm=na.rm, simplify=simplify, FUN="viewSums", FUN.TYPE="numeric")
+            .do_rledf_views(x, ir, na.rm=na.rm, simplify=simplify, RLEFUN=.rle_view_sums, FUN.TYPE="numeric")
           })
 
 ##' @export
 setGeneric("rangeMeans", function(x, ir, na.rm=TRUE, simplify=TRUE) { standardGeneric("rangeMeans") })
 setMethod("rangeMeans", signature=signature(x="RleDataFrame"), 
           function(x, ir, na.rm=TRUE, simplify=TRUE) {
-            .do_rledf_views(x, ir, na.rm=na.rm, simplify=simplify, FUN="viewMeans", FUN.TYPE="numeric")
+            .do_rledf_views(x, ir, na.rm=na.rm, simplify=simplify, RLEFUN=.rle_view_means, FUN.TYPE="numeric")
           })
 
 ##' @export
 setGeneric("rangeMins", function(x, ir, na.rm=TRUE, simplify=TRUE) { standardGeneric("rangeMins") })
 setMethod("rangeMins", signature=signature(x="RleDataFrame"), 
           function(x, ir, na.rm=TRUE, simplify=TRUE) {
-            .do_rledf_views(x, ir, na.rm=na.rm, simplify=simplify, FUN="viewMins", FUN.TYPE="numeric")
+            .do_rledf_views(x, ir, na.rm=na.rm, simplify=simplify, RLEFUN=.rle_view_mins, FUN.TYPE="numeric")
           })
 
 ##' @export
 setGeneric("rangeMaxs", function(x, ir, na.rm=TRUE, simplify=TRUE) { standardGeneric("rangeMaxs") })
 setMethod("rangeMaxs", signature=signature(x="RleDataFrame"), 
           function(x, ir, na.rm=TRUE, simplify=TRUE) {
-            .do_rledf_views(x, ir, na.rm=na.rm, simplify=simplify, FUN="viewMaxs", FUN.TYPE="numeric")
+            .do_rledf_views(x, ir, na.rm=na.rm, simplify=simplify, RLEFUN=.rle_view_maxs, FUN.TYPE="numeric")
           })
 
 ##' @export
 setGeneric("rangeWhichMins", function(x, ir, na.rm=TRUE, simplify=TRUE) { standardGeneric("rangeWhichMins") })
 setMethod("rangeWhichMins", signature=signature(x="RleDataFrame"), 
           function(x, ir, na.rm=TRUE, simplify=TRUE) {
-            .do_rledf_views(x, ir, na.rm=na.rm, simplify=simplify, FUN="viewWhichMins", FUN.TYPE="integer")
+            .do_rledf_views(x, ir, na.rm=na.rm, simplify=simplify, RLEFUN=.rle_view_which_mins, FUN.TYPE="integer")
           })
 
 ##' @export
 setGeneric("rangeWhichMaxs", function(x, ir, na.rm=TRUE, simplify=TRUE) { standardGeneric("rangeWhichMaxs") })
 setMethod("rangeWhichMaxs", signature=signature(x="RleDataFrame"), 
           function(x, ir, na.rm=TRUE, simplify=TRUE) {
-            .do_rledf_views(x, ir, na.rm=na.rm, simplify=simplify, FUN="viewWhichMaxs", FUN.TYPE="integer")
+            .do_rledf_views(x, ir, na.rm=na.rm, simplify=simplify, RLEFUN=.rle_view_which_maxs, FUN.TYPE="integer")
           })
 
+
+### Internal methods to get directly to summary functions, skipping trim
+.rle_view_sums <- function(x, na.rm) { .Call2("RleViews_viewSums", x, na.rm, PACKAGE = "IRanges") }
+.rle_view_means <- function(x, na.rm) { .Call2("RleViews_viewMeans", x, na.rm, PACKAGE = "IRanges") }
+.rle_view_mins <- function(x, na.rm) { .Call2("RleViews_viewMins", x, na.rm, PACKAGE = "IRanges") }
+.rle_view_maxs <- function(x, na.rm) { .Call2("RleViews_viewMaxs", x, na.rm, PACKAGE = "IRanges") }
+.rle_view_which_mins <- function(x, na.rm) { .Call2("RleViews_viewWhichMins", x, na.rm, PACKAGE = "IRanges") }
+.rle_view_which_maxs <- function(x, na.rm) { .Call2("RleViews_viewWhichMaxs", x, na.rm, PACKAGE = "IRanges") }
