81119da8b1a0062ad547611103d577d2b0256a7b
Peter M Haverty <phaverty@gene.com>
2014-05-14 20:22:26 +0000

    Version: 1.19.15. Oops, left out genoset.h
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/genoset@90349 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ src/genoset.h
@@ -0,0 +1,17 @@
+#ifndef GENOSET_H
+#define GENOSET_H
+
+// utils.c
+void isNA(SEXP vec, char* na);
+void widthToStartEnd(int* width, double* start, double* end, int n);
+
+// views.c
+SEXP RleViews_viewMeans(SEXP Start, SEXP Width, SEXP Values, SEXP Lengths, SEXP Na_rm);
+
+// genoset.c
+void binary_bound( int *starts, int *stops, int *positions, int *num_queries, int *num_positions, int *bounds, int *valid_indices, int *offset );
+SEXP rangeColMeans( SEXP bounds, SEXP x );
+SEXP binary_bound_by_chr(SEXP nquery, SEXP query_chr_indices, SEXP query_starts, SEXP query_ends, SEXP query_names, SEXP subject_chr_indices, SEXP subject_starts, SEXP subject_ends);
+
+#endif
+
