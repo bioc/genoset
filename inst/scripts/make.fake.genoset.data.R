477e7e7f846df3991af767dc3a7a327209d66d37
Peter M Haverty <phaverty@gene.com>
2016-01-29 04:16:11 +0000

    Version: 1.27.12. updated stored data
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/genoset@113049 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/scripts/make.fake.genoset.data.R
@@ -0,0 +1,36 @@
+#!/usr/bin/env Rscript
+
+library(genoset)
+library(GenomicRanges)
+library(SummarizedExperiment)
+
+sample.names = LETTERS[11:13]
+probe.names = paste("p", 1:1000, sep="")
+num.samples = length(sample.names)
+num.probes  = length(probe.names)
+
+locData.gr = GRanges(
+  ranges= IRanges(
+    start=c(seq(from=125e6,by=3e4,length=400), seq(from=1,length=400,by=3.25e4),seq(from=30e6,length=200,by=3e4)),width=1,names=probe.names),
+  seqnames=factor(c(rep("chr8",400), rep("chr12",400),rep("chr17",200)),levels=c("chr8","chr12","chr17")))
+
+genome(locData.gr) = "hg19"
+fake.baf  = sample(c(0,0.5,1), length(probe.names), replace=TRUE) + rnorm(length(probe.names),0,0.01)
+fake.baf[ fake.baf > 1 ] = fake.baf[ fake.baf > 1 ] - 1
+fake.baf[ fake.baf < 0 ] = fake.baf[ fake.baf < 0 ] + 1
+hets = fake.baf < 0.75 & fake.baf > 0.25
+fake.baf[ 101:200 ][ hets[101:200] ] = fake.baf[ 101:200 ][ hets[101:200] ] + rep(c(-0.2,0.2),50)[hets[101:200]]
+fake.lrr = matrix(c(
+  c(rnorm(200,.4,0.05),rnorm(200,.2,0.05),rnorm(200,0.23,0.05),rnorm(200,.15,0.05),rnorm(200,2.,0.05)), 
+  c(rnorm(200,0,0.05), rnorm(200,3,0.05),rnorm(200,14,0.05),rnorm(200,.1,0.05),rnorm(200,-0.05,0.05)),
+  c(rnorm(200,.1,0.05),rnorm(200,1,0.05),rnorm(200,-.5,0.05),rnorm(200,3,0.05),rnorm(200,3,0.05))
+  ),
+  nrow=num.probes,ncol=num.samples,dimnames=list(probe.names,sample.names))
+fake.baf = matrix(fake.baf,nrow=num.probes,ncol=num.samples,dimnames=list(probe.names,sample.names))
+fake.pData=data.frame(matrix(LETTERS[1:15],nrow=3,ncol=5,dimnames=list(sample.names,letters[1:5])))
+fake.cn = fake.lrr
+
+assays = Assays(list(lrr=fake.lrr, baf=fake.baf))
+genoset.ds = GenoSet( locData.gr, assays, fake.pData )
+
+save(locData.gr, fake.baf, fake.lrr, fake.cn, fake.pData, genoset.ds, file="genoset.RData", compression_level=9, compress="xz")
