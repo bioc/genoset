fe2a9d3c4f27da9e68d50f72a53aa99720ca242e
Peter M Haverty <phaverty@gene.com>
2016-08-23 04:05:56 +0000

    Version: 1.29.9. no more startup message
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/genoset@120356 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++
@@ -1,4 +0,0 @@
-.onAttach <- function(libname, pkgname) {
-    packageStartupMessage("\n*** Genoset API Changes ***\nThe genoset class has transitioned to the
-RangedSummarizedExperiment API from the eSet API (e.g. use colnames instead of sampleNames). ***")
-}
